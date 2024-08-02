87b54b000073260e91c291a518cd388f44cf50ff
DaniTech <tochidan11@gmail.com>
2024-08-02 12:06:32 -0700

    Insalled etherjs

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <h1>Today I am lerning EtherJs(everything)</h1>
+      <h2>What is EtherJs?</h2>
+      hello
+    </div>
+  );
+};
+
+export default index;
