6f5902025da4526a535c31262baaee8a0b2a7c5b
DaniTech <tochidan11@gmail.com>
2024-08-02 22:09:28 -0700

    Getting the default etherscan provider

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
 const index = () => {
+  // const [currentAccount, setCurrentAccount] = useState("");
+  const address = "0x4838B106FCe9647Bdf1E7877BF73cE8B0BAD5f97";
+
+  const provider = new ethers.providers.EtherscanProvider();
+
+  //to see the balance of this account
+
+  const balance = console.log("provider", provider);
+
   return (
     <div>
       <h1>Today I am lerning EtherJs(everything)</h1>
