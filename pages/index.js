0c6c49f83fa1894219e355636135e3a7c94f0627
DaniTech <tochidan11@gmail.com>
2024-08-02 22:40:53 -0700

    getBlalance method of the default provider(EtherscanProvider)

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,17 @@ import { ethers } from "ethers";
 const index = () => {
   // const [currentAccount, setCurrentAccount] = useState("");
   const address = "0x4838B106FCe9647Bdf1E7877BF73cE8B0BAD5f97";
+  const apiKey = "WS6U15YJ8GRAWGSX72DT13F7R85QDXR9FU"; // replace with your API key
 
-  const provider = new ethers.providers.EtherscanProvider();
+  const etherjs = async () => {
+    const provider = new ethers.providers.EtherscanProvider("mainnet", apiKey);
+    const balance = await provider.getBalance(address);
+    console.log("balance", balance);
+  };
 
-  //to see the balance of this account
-
-  const balance = console.log("provider", provider);
-
+  useEffect(() => {
+    etherjs();
+  }, []);
   return (
     <div>
       <h1>Today I am lerning EtherJs(everything)</h1>
